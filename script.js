b119531059022e70a11b7a98420918f137cfc49b
alyyokomichi <alyyokomichi@gmail.com>
2020-11-20 18:49:20 -0300

    definir fim do jogo

--- script.js
+++ script.js
@@ -28,8 +28,6 @@ function drawFood(){
     context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
-
 function update(event){
     if(event.keyCode == 37 && direction !="right") direction = "left";
     if(event.keyCode == 38 && direction !="down") direction = "up";
@@ -38,12 +36,21 @@ function update(event){
     
 }
 
+document.addEventListener('keydown', update);
+
 function iniciarJogo(){
 
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y > 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i< snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game over! :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -63,8 +70,6 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-    snake.pop();
 
     let newHead = {
         x: snakeX,
